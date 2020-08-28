fc53c49116c36b2bdf1559cf932f2f7930695362
dongdl <dongdongliang13@hotmail.com>
2020-08-28 12:07:19 +0800

    update: 代码雨 封装成组件

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,5 +1,6 @@
 import commonForm from '../../plugin/commonForm'
 import {isLogin} from '../../plugin/storage'
+import {rainy} from '../../plugin/codeRain'
 
 export default {
   name: 'LOGIN',
@@ -37,33 +38,6 @@ export default {
         ]
       );
     },
-    code_rain() {
-      let can = document.createElement("canvas");
-      can.id = "canvas";
-      can.style.position="fixed";
-      can.style.left="0";
-      can.style.top="0";
-      can.style.zIndex="-1";
-      can.style.opacity="1";
-      document.body.appendChild(can);
-
-      let width = document.getElementById("canvas").width = screen.width;
-      let height = document.getElementById("canvas").height = screen.height;
-      let ctx = document.getElementById("canvas").getContext("2d");
-      let arr = Array(Math.ceil(width / 10)).fill(0);
-      let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".split("");
-
-      function rain() {
-        ctx.fillStyle = "rgba(0,0,0,0.05)";
-        ctx.fillRect(0, 0, width, height);
-        ctx.fillStyle = "#0f0";
-        arr.forEach(function (value, index) {
-          ctx.fillText(str[Math.floor(Math.random() * str.length)], index * 10, value + 10);
-          arr[index] = value >= height || value > 8888 * Math.random() ? 0 : value + 10;
-        });
-      }
-      setInterval(rain, 30);
-    }
   },
   render(h) {
     return h('div', {
@@ -77,13 +51,12 @@ export default {
         // },
       },
       [
-        this.code_rain(),
+        rainy(),
         this.render_login(h)
       ]
     );
   },
   created() {
-    // this.code_rain();
     if (isLogin()) {
       this.$router.push('/');
     }
---
+++ src/plugin/codeRain.js
@@ -0,0 +1,32 @@
+let can = document.createElement("canvas");
+can.id = "canvas";
+can.style.position="fixed";
+can.style.left="0";
+can.style.top="0";
+can.style.zIndex="-1";
+can.style.opacity="1";
+document.body.appendChild(can);
+
+let width = document.getElementById("canvas").width = screen.width;
+let height = document.getElementById("canvas").height = screen.height;
+let ctx = document.getElementById("canvas").getContext("2d");
+let arr = Array(Math.ceil(width / 10)).fill(0);
+let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".split("");
+
+function rain() {
+  ctx.fillStyle = "rgba(0,0,0,0.05)";
+  ctx.fillRect(0, 0, width, height);
+  ctx.fillStyle = "#0f0";
+  arr.forEach(function (value, index) {
+    ctx.fillText(str[Math.floor(Math.random() * str.length)], index * 10, value + 10);
+    arr[index] = value >= height || value > 8888 * Math.random() ? 0 : value + 10;
+  });
+}
+
+function rainy() {
+  setInterval(rain, 30);
+}
+
+export {
+  rainy
+}
