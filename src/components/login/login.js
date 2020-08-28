48747a6f1f555da5d0809d26c9664f6850ab9fdf
dongdl <dongdongliang13@hotmail.com>
2020-08-28 09:55:51 +0800

    update: 添加代码雨背景， 登录框位置自动适应

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -17,10 +17,13 @@ export default {
       return h('div', {
           style: {
             'background-color': 'gray',
-            padding: '50px',
-            margin: '250px auto',
+            padding: '40px',
+            position: 'fixed',
+            left: '50%',
+            top: '50%',
+            margin: '-125px -175px',
             width: '350px',
-            opacity: '0.75'
+            opacity: '0.9'
           }
         }, [
           h('commonForm', {
@@ -33,26 +36,54 @@ export default {
           }, [])
         ]
       );
+    },
+    code_rain() {
+      let can = document.createElement("canvas");
+      can.id = "canvas";
+      can.style.position="fixed";
+      can.style.left="0";
+      can.style.top="0";
+      can.style.zIndex="-1";
+      can.style.opacity="1";
+      document.body.appendChild(can);
+
+      let width = document.getElementById("canvas").width = screen.width;
+      let height = document.getElementById("canvas").height = screen.height;
+      let ctx = document.getElementById("canvas").getContext("2d");
+      let arr = Array(Math.ceil(width / 10)).fill(0);
+      let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".split("");
+
+      function rain() {
+        ctx.fillStyle = "rgba(0,0,0,0.05)";
+        ctx.fillRect(0, 0, width, height);
+        ctx.fillStyle = "#0f0";
+        arr.forEach(function (value, index) {
+          ctx.fillText(str[Math.floor(Math.random() * str.length)], index * 10, value + 10);
+          arr[index] = value >= height || value > 8888 * Math.random() ? 0 : value + 10;
+        });
+      }
+      setInterval(rain, 30);
     }
   },
   render(h) {
     return h('div', {
-        style: {
-          background: 'url(' + require('../../assets/bg1.jpg') + ') no-repeat center center',
-          position: 'fixed',
-          top: 0,
-          left: 0,
-          width: '100%',
-          height: '100%',
-        },
+        // style: {
+        //   background: 'url(' + require('../../assets/bg1.jpg') + ') no-repeat center center',
+        //   position: 'fixed',
+        //   top: 0,
+        //   left: 0,
+        //   width: '100%',
+        //   height: '100%',
+        // },
       },
       [
+        this.code_rain(),
         this.render_login(h)
       ]
-    )
-      ;
+    );
   },
   created() {
+    // this.code_rain();
     if (isLogin()) {
       this.$router.push('/');
     }
